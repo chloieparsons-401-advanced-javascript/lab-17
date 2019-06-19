97ac29ebec200c35fd312bc1cce7e2f09548150c
Chloie Parsons <chloieparsons@gmail.com>
2019-06-18 23:51:30 -0700

    README Updated

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # lab-17
 TCP Server / Message Application
+
+##Author: Chloie Parsons 
+
+## Links and Resources
+* submission PR: https://github.com/chloieparsons-401-advanced-javascript/lab-17/pull/1
+* Travis: https://www.travis-ci.com/chloieparsons-401-advanced-javascript/lab-17
+
+
+## Modules
+* app.js
+* logger.js
+* server.js
+
+## Setup
+### .env requirements
+PORT - 3001
+
+## Running the app
+* npm start script (node app.js)
+Endpoint: /signup
+Endpoint: /signin
+
+## Tests
+* npm run test
+* npm run test-watch
+* npm run lint
+
+##UML
+![TCP](assets/TCP_UML.JPG)
--- logger.js
+++ logger.js
@@ -2,13 +2,22 @@
 
 const net = require('net');
 
-const client = new  net.Socket();
+const client = new net.Socket();
 
-client.connect(3001, 'localhost'() => console.log('Socket in logger.js created!'));
+client.connect(3001, 'localhost', () => console.log('Socket in logger.js created!'));
 
+// Listener
 client.on('data', data => {
   let payload = JSON.parse(data.toString().trim());
   console.log(payload)
+});
+
+client.on('error', () => {
+  console.log('Connection to logger socket closed');
+});
+
+client.on('save', () => {
+  console.log('Connection to logger socket closed');
 });
 
 client.on('close', () => {
