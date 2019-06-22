69061f21f9306aed58b6b3815025c949613e6f16
Chloie Parsons <chloieparsons@gmail.com>
2019-06-22 12:17:12 -0700

    Working on TCP connection

--- server.js
+++ server.js
@@ -8,29 +8,20 @@ const server = net.createServer();
 
 server.listen(port, () => console.log(`Server up on ${port}`) );
 
-let allowedEvents = ['save', 'error'];
 let socketPool = {};
 
 server.on('connection', (socket) => {
   const id = `Socket-${Math.random()}`;
   socketPool[id] = socket;
-  socket.on('data', (buffer) => dispatchEvent(buffer));
+  socket.on('data', (buffer) => events.emit('dispatchEvent',buffer));
   socket.on('close', () => {
     delete socketPool[id];
   });
 });
 
 let dispatchEvent = (buffer) => {
-  let text = buffer.toString().trim();
-  let payload = JSON.parse(text);
-
-  if(allowedEvents.includes(payload.name)){
-    console.log(`BROADCAST: ${payload.name}`);
-    for (let socket in socketPool) {
-      socketPool[socket].write(JSON.stringify(payload));
-    }
-  } else {
-    console.log(`IGNORE ${payload.name}`);
+  for (let socket in socketPool) {
+    socketPool[socket].write(buffer);
   }
 };
 
